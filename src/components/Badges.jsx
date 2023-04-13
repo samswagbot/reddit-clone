8a0fcf989bbe6ef225cd1043ecb3ad3ae7e8d369
Samson <sammossallam@gmail.com>
2023-04-13 19:30:30 -0400

    Badges component

---
+++ src/components/Badges.jsx
@@ -0,0 +1,9 @@
+export default function Badges({ award }) {
+  const { icon_url, name } = award;
+  return (
+    <div className="text-center font-bold">
+      <span className="text-xs">{name}</span>
+      <img src={icon_url} alt={name} className=" inline-block w-4 h-4 ml-1" />
+    </div>
+  );
+}
