c7fb7bd32b90f2ab57ecd8c5ad6381d7f7056331
Samson <sammossallam@gmail.com>
2023-04-13 20:14:37 -0400

    util for pics

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import CommentOutlinedIcon from "@mui/icons-material/CommentOutlined";
 import SwapVertOutlined from "@mui/icons-material/SwapVertOutlined";
 import ImageNotSupportedIcon from "@mui/icons-material/ImageNotSupported";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { checkIfUrlIsImg } from "../utlis";
 
 export default function Tile(props) {
   const {
@@ -14,7 +16,9 @@ export default function Tile(props) {
     num_comments,
     subreddit,
     score,
+    url,
   } = props;
+  const isSmallScreen = useMediaQuery("(max-width: 600px)");
   const createdDaysAgo = useMemo(() => {
     const createdAt = new Date(created_utc * 1000);
     const now = new Date();
@@ -34,8 +38,8 @@ export default function Tile(props) {
 
   return (
     <Link
-      state={{ props }}
-      className="flex flex-col md:flex-row pointer md:w-3/5 mx-auto mt-6 shadow-xl rounded-lg hover:transform hover:scale-105 hover:ease-in-out bg-white hover:bg-gray-200"
+      state={{ post: props }}
+      className="flex flex-col md:flex-row pointer lg:w-3/5 mx-auto mt-6 shadow-xl rounded-lg hover:transform hover:scale-105 hover:ease-in-out bg-white hover:bg-gray-200"
       to={`/post/${id}`}
     >
       <div className="mr-5 flex flex-col justify-center space-y-2 w-full pl-4 pt-4 pb-4">
@@ -62,6 +66,7 @@ export default function Tile(props) {
           </span>
         </div>
       </div>
+      {console.log(thumbnail, title)}
       {thumbnail === "nsfw" ? (
         <div className="w-36 hidden pr-5 md:flex items-center flex-col justify-center">
           <ImageNotSupportedIcon />
@@ -69,8 +74,8 @@ export default function Tile(props) {
         </div>
       ) : (
         <img
-          className="md:rounded-tr-lg rounded-bl-lg md:rounded-bl-none h-auto rounded-br-lg "
-          src={thumbnail}
+          className="md:rounded-tr-lg rounded-bl-lg md:rounded-bl-none max-h-96 h-auto rounded-br-lg "
+          src={isSmallScreen && checkIfUrlIsImg(url) ? url : thumbnail}
           alt={title}
         />
       )}
---
+++ src/utlis/index.js
@@ -0,0 +1,2 @@
+// some urls are links to galleries instead of images
+export const checkIfUrlIsImg = (link) => link.endsWith(".jpg");
